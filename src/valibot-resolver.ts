865946d5f5c318e77dce55fa67d041e5b789ca07
Adel Ak <adelak@Adels-MacBook-Pro.local>
2024-08-22 11:34:48 +0300

    feat: pass config to parse

--- src/valibot-resolver.ts
+++ src/valibot-resolver.ts
@@ -1,5 +1,12 @@
-import type { FormErrors } from '@mantine/form'
-import { type BaseIssue, type BaseSchema, getDotPath, safeParse } from 'valibot'
+import type { FormErrors } from "@mantine/form";
+import {
+  type BaseIssue,
+  type BaseSchema,
+  Config,
+  getDotPath,
+  InferIssue,
+  safeParse,
+} from "valibot";
 
 /**
  * Returns a function that can be used to validate a Maninte form with Valibot.
@@ -9,19 +16,20 @@ import { type BaseIssue, type BaseSchema, getDotPath, safeParse } from 'valibot'
  * @returns A validation function.
  */
 export function valibotResolver<TValues extends Record<string, unknown>>(
-	schema: BaseSchema<TValues, TValues, BaseIssue<unknown>>,
+  schema: BaseSchema<TValues, TValues, BaseIssue<unknown>>,
+  config?: Config<InferIssue<typeof schema>>
 ) {
-	return (values: TValues): FormErrors => {
-		const errors: FormErrors = {}
-		const result = safeParse(schema, values)
-		if (result.issues) {
-			for (const issue of result.issues) {
-				const path = getDotPath(issue)
-				if (path) {
-					errors[path] = issue.message
-				}
-			}
-		}
-		return errors
-	}
+  return (values: TValues): FormErrors => {
+    const errors: FormErrors = {};
+    const result = safeParse(schema, values, config);
+    if (result.issues) {
+      for (const issue of result.issues) {
+        const path = getDotPath(issue);
+        if (path) {
+          errors[path] = issue.message;
+        }
+      }
+    }
+    return errors;
+  };
 }
